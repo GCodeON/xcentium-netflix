71c6781a79912d341f1a331a63c48dfeaa8d8e0e
GCodeON <gcodeondev@gmail.com>
2024-01-05 01:11:55 -0800

    slider update

--- src/components/slider.js
+++ src/components/slider.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 import '@/scss/slider.scss';
 
 export default function Slider({query}) {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState();
 
   const apiURL = `https://www.omdbapi.com/?apiKey=${process.env.NEXT_PUBLIC_OMDB_KEY}`
 
@@ -24,25 +24,27 @@ export default function Slider({query}) {
   
   return (
     <div className="slider">
-      <h2 className="title">{query}</h2>
-      <ul className='movies'>
-        { movies && (
-          movies.map((movie, idx) => {
-            return (
-              <li key={movie.imdbID}>
-                <Link className="link" href={`/movie/details/${movie.imdbID}`}>
-                  <Image className="poster" 
-                    src={movie.Poster} 
-                    alt={movie.Title}
-                    width={300}
-                    height={444}
-                  />
-                </Link>
-              </li>
-            )
-          })
-        )}
-      </ul>
+      { movies && (
+        <>
+          <h2 className="title">{query}</h2>
+          <ul className='movies'>
+            { movies.map((movie, idx) => {
+                return (
+                  <li key={movie.imdbID}>
+                    <Link className="link" href={`/movie/details/${movie.imdbID}`}>
+                      <Image className="poster" 
+                        src={movie.Poster} 
+                        alt={movie.Title}
+                        width={300}
+                        height={444}
+                      />
+                    </Link>
+                  </li>
+                )
+              })}
+          </ul>
+        </>
+      )}
     </div>
   )
 }
